2cf10018fd6587ffe25251dd0333e2f1e6d9dc83
sivaserr <sivabeec95@gmail.com>
2019-10-12 15:51:11 +0530

    updated

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        // $posts = Post::all();
+        $posts = Post::orderBy('title','desc')->paginate(1);
         return view('posts.index')->with('posts' , $posts);
     }
 
